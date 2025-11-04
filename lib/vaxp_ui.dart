4db6b0af4f7db498a4cfa018644a3315671f901e
Your Name <you@example.com>
2025-11-04 12:17:05 +0300

    u

--- lib/vaxp_ui.dart
+++ lib/vaxp_ui.dart
@@ -1,3 +1,11 @@
+library vaxp_ui;
+export 'theme/vaxp_theme.dart';
+export 'components/buttons/vaxp_button.dart';
+export 'components/cards/vaxp_card.dart';
+export 'components/navigation/vaxp_appbar.dart';
+export 'icons/vaxp_icons.dart';
+
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
